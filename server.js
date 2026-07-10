38a791dea1459171fb5605719e04b694134f9b1a
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-07-11 00:31:34 +0530

    Configure server for Render deployment

--- server.js
+++ server.js
@@ -562,8 +562,8 @@ function updateUsers(room) {
 
 }
 
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-    console.log("Server running on port 3000");
-
-});
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
