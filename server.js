657927aa2868e426e4d99a7af944047b723a4b56
hariomCodes31 <hariomsingh9696822@gmail.com>
2026-08-20 22:53:47 +0530

    Fix Render server binding

--- server.js
+++ server.js
@@ -827,6 +827,6 @@ function cleanupUserSession(socketId, user) {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
